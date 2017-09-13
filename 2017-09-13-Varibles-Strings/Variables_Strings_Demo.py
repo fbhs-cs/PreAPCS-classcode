47ac202c0772cb3c61175d89bc7854db2898f744
Purdy <374266@flourbluffschools.net>
2017-09-13 08:22:01 -0500

    pre-class

---
+++ 2017-09-13-Varibles-Strings/Variables_Strings_Demo.py
@@ -0,0 +1,17 @@
+
+#This is a one-line comment
+'''This is a longer comment that can
+   continue over multiple lines.  We will use
+   this type of comment to describe many of
+   the functions that we create'''
+
+
+#Assignment statements
+#Variable names
+#Keywords
+#Expressions and statements
+#Order of operations
+#Basic string operations
+#Types of errors
+
+
