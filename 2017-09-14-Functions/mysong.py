9948ec97d314c4413b8d4fc16af691c3d616faee
Purdy <374266@flourbluffschools.net>
2017-09-18 07:36:05 -0500

    added and removed stuff

---
+++ 2017-09-14-Functions/mysong.py
@@ -0,0 +1,14 @@
+'''MAKE A NEW FILE CALLED: mysong.py'''
+
+'''Your program must include at least 3 functions:
+    sing()
+    verse()
+    one_other()
+
+   At least ONE of your functions must take an
+   argument - verse.
+   
+   I should be able to call the verse() function
+   in sing() using an argument (or arguments)
+   of my choice.
+'''
