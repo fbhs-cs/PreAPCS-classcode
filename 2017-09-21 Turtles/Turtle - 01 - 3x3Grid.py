70c36eb9acbdc36669bcdb9c4460e3e6c70f6544
Chad Purdy <chpurdy@gmail.com>
2017-09-20 22:10:39 -0500

    new files

---
+++ 2017-09-21 Turtles/Turtle - 01 - 3x3Grid.py
@@ -0,0 +1,19 @@
+#NAME:
+#DATE:
+#PERIOD:
+
+"""DO NOT EDIT ANYTHING IN THIS SECTION"""
+import turtle
+
+t = turtle.Turtle()
+t.seth(90)
+
+def moveForward():
+    t.fd(50)
+    
+def turnLeft():
+    t.lt(90)
+    
+"""-----------------------------------"""
+
+#Start your code here
