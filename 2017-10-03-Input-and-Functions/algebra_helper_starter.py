0128d2242417e1f14711875455a4c88602fc899b
Chad Purdy <cpurdy@flourbluffschools.net>
2017-10-05 07:57:07 -0500

    starter code

---
+++ 2017-10-03-Input-and-Functions/algebra_helper_starter.py
@@ -0,0 +1,37 @@
+''' Here is some starter code to help you
+    with the algebra_helper project '''
+
+import math
+
+def find_slope(x1,y1,x2,y2):
+    m = (y2 - y1) / (x2 - x1)
+    print('The slope of the line through ({}, {}) and ({}, {}) is {}'.format(x1,y1,x2,y2,m))
+    
+def find_midpoint(x1,y1,x2,y2):
+    #calculations for midpoint
+    print()
+    
+def find_length(x1,y1,x2,y2):
+    #calculations for length
+    print()
+    
+def find_equation(x1,y1,x2,y2):
+    #calculations for equation
+    print()
+    
+
+print('Welcome!')  #make this more friendly
+name = input('Name: ') #make this more friendly
+print('Personal greeting.') #make this more personal
+print('I need stuff') #Explain what is needed
+x_1 = float(input('x1 = '))
+y_1 = float(input('y1 = '))
+x_2 = float(input('x2 = '))
+y_2 = float(input('y2 = '))
+
+find_slope(x_1,y_1,x_2,y_2)
+# call other functions
+
+          
+          
+        
