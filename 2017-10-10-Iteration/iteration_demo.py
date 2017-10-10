250de204c16f87f054470087c83d9983a86c50ad
Chad Purdy <cpurdy@flourbluffschools.net>
2017-10-10 10:30:40 -0500

    removed function:

---
+++ 2017-10-10-Iteration/iteration_demo.py
@@ -0,0 +1,9 @@
+num1 = int(input( "Number 1: " ))
+num2 = int(input( "Number 2: " ))
+num3 = int(input( "Number 3: " ))
+num4 = int(input( "Number 4: " ))
+num5 = int(input( "Number 5: " ))
+
+print( "Total is", num1 + num2 + num3 + num4 + num5 )
+    
+
