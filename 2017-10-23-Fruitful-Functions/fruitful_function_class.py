1ba38e205a85c58eec3aaf7cbd2897885ed7ff88
Chad Purdy <cpurdy@flourbluffschools.net>
2017-10-23 08:34:17 -0500

    starter code

---
+++ 2017-10-23-Fruitful-Functions/fruitful_function_class.py
@@ -0,0 +1,57 @@
+'''BEFORE WE BEGIN, GO TO FILE...SAVE AS
+   AND SAVE THIS FILE AS fruitful_function_mine.py
+   THEN DELETE THIS COMMENT
+'''
+
+def area(radius):
+    '''returns the area of a circle with radius
+    '''
+    return 0.0
+
+
+def absolute_value(x):
+    '''returns the absolute value of x
+    '''
+    
+
+def compare(x, y):
+    '''returns 1 if x is greater than  y
+       returns 0 if x is equal to y
+       returns -1 if x is less than y
+    '''
+    return 0
+
+
+def distance(x1,y1,x2,y2):
+    '''returns the distance between points (x1,y1) and (x2, y2)
+    '''
+    return 0.0
+
+
+
+
+def circle_area(xc,yc,xp,yp):
+    '''returns the area of a circle with center at (xc, yc)
+       and a point on the perimeter of the circle (xp, yp)
+    '''
+    return 0.0
+
+
+
+def is_divisible(x, y):
+    '''returns True if x is divisible by y
+       and False otherwise
+    '''
+    return False
+
+
+def is_between(x,y,z):
+    '''returns True if y is between x and z
+       that is, x <= y <= z,
+       returns False otherwise
+    '''
+    
+
+                   
+
+
