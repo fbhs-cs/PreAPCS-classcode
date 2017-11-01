f1fb1c5907ce7c0920db040abbd85e1b92f7d13c
Chad Purdy <cpurdy@flourbluffschools.net>
2017-11-01 10:39:45 -0500

    rps

--- 2017-11-01-RPS/rps_math.py
+++ 2017-11-01-RPS/rps_math.py
@@ -3,11 +3,3 @@ for player in range(3):
         diff = player - computer
         result = diff % 3
         print('Player: {}\tComputer: {}\tResult: {}'.format(player,computer,result))
-
-if pc == 'R' and cc == 'R':
-
-elif pc == 'R' and cc == 'P':
-    
-elif pc == 'R' and cc = 'S':
-    
-elif 
