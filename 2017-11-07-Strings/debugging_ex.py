9f4174db5a4838a50b08f25c296298b06852e506
Chad Purdy <chpurdy@gmail.com>
2017-11-12 20:22:51 -0600

    updated example

--- 2017-11-07-Strings/debugging_ex.py
+++ 2017-11-07-Strings/debugging_ex.py
@@ -11,23 +11,3 @@ def is_reverse(word1, word2):
         i = i+1
         j= j-1
     return True
-
-def is_palindrome(word):
-    if len(word) == 0:
-        return False
-    i = 0
-    j = len(word) - 1
-    while j >= 0:
-        if word[i].lower() != word[j].lower():
-            return False
-        i = i+1
-        j = j-1
-    return True
-
-
-def what_is_after(word,letter):
-    num_times = word.count(letter)
-    is_at = -1
-    for i in range(num_times):
-        is_at = word.find(letter,is_at+1)
-    return word[is_at+1:]
