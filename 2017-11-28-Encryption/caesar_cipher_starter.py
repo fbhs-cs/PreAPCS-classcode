d8f537fd30004550ed5deb0bc1dcf8c114f4350d
Chad Purdy <cpurdy@flourbluffschools.net>
2017-11-29 07:58:54 -0600

    caesar cipher init

---
+++ 2017-11-28-Encryption/caesar_cipher_starter.py
@@ -0,0 +1,33 @@
+'''
+BEFORE WE START:
+GO TO FILE...SAVE AS...and save this file as
+caesar_cipher.py
+
+THEN DELETE THIS COMMENT
+'''
+
+'''
+Encryption Algorithm
+1. Decide on a key from 1 to 25.
+2. Find the plaintext letter's number
+3. Add the key to the plaintext letter's number
+4. If this number is larger than 26, subtract 26.
+5. Find the letter for the number calculated. This
+   is the cipher text letter.
+6. Repeat steps 2 to 5 for every letter in the plaintext
+   message.
+'''
+
+
+'''
+Decryption Algorithm
+1. Given a key from 1 to 25.
+2. Find the ciphertext letter's number
+3. Subtract the key from the ciphertext letter's number
+4. If this number is negative, add 26.
+5. Find the letter for the number calculated. This
+   is the plaintext letter.
+6. Repeat steps 2 to 5 for every letter in the ciphertext
+   message.
+'''
+
